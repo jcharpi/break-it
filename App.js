4a9f46cba20cb9ec80acdedb4211a0f5084bffec
Josh Charpentier <charpentier.josh@gmail.com>
2023-04-02 23:56:57 -0500

    navigation woot woot

--- App.js
+++ App.js
@@ -1,35 +1,35 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { useTheme } from 'react-native-paper';
 import CreateHabitLayout from './layouts/CreateHabitLayout';
 import ProgressPage from './pages/ProgressPage';
 import TrovePage from './pages/TrovePage';
 
 export default function App() {
-  const Stack = createStackNavigator()
+  const Tab = createMaterialTopTabNavigator();
   const theme = useTheme();
   theme.colors.secondaryContainer = "transperent"
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='CreateHabitLayout'>
-        <Stack.Screen
+      <Tab.Navigator 
+        initialRouteName='CreateHabitLayout'
+        tabBar={() => null}>
+        <Tab.Screen
           name="CreateHabitLayout"
           component={CreateHabitLayout}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
+          options={{ swipeEnabled: false }}
+          />
+        <Tab.Screen
           name="ProgressPage"
           component={ProgressPage}
-          options={{ headerShown: false }}
         />
-        <Stack.Screen
+        <Tab.Screen
           name="TrovePage"
           component={TrovePage}
-          options={{ headerShown: false }}
         />
        
-      </Stack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- layouts/CreateHabitLayout.tsx
+++ layouts/CreateHabitLayout.tsx
@@ -1,4 +1,4 @@
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { StyleSheet } from "react-native";
 import EnterHabitPage from "../pages/EnterHabitPage";
 import QuestionPage from "../pages/QuestionPage";
@@ -6,23 +6,29 @@ import WhatNowPage from "../pages/WhatNowPage";
 import Icon from 'react-native-vector-icons/Ionicons'
 
 export default function CreateHabitLayout() {
-    const Tab = createMaterialBottomTabNavigator();
+    const Tab = createMaterialTopTabNavigator();
 
     return (
         <Tab.Navigator 
             initialRouteName='Enter Habit' 
             backBehavior='history' 
-            labeled={false}
-            barStyle={styles.tabBar}
-            screenOptions={() => ({
-            tabBarIcon: ({ focused, color }) => {
-                let iconName;          
-                iconName = focused ? 'ellipse' : 'ellipse-outline';
-                color = focused ? '#DDE2F5' : 'white'
-                // You can return any component that you like here!
-                return <Icon name={iconName} size={27} color={color} />;
-            },
-            })}
+            tabBarPosition='bottom'
+            screenOptions={{
+                tabBarIcon: ({ focused, color }) => {
+                    let iconName;          
+                    iconName = focused ? 'ellipse' : 'ellipse-outline';
+                    color = focused ? '#DDE2F5' : 'white'
+                    // You can return any component that you like here!
+                    return <Icon name={iconName} size={25} color={color} />;
+                },
+                tabBarShowLabel: false,
+                tabBarStyle: styles.tabBar,
+                tabBarContentContainerStyle: styles.tabBarContainer,
+                tabBarItemStyle: styles.tabBarItem,
+                tabBarIndicatorStyle: { opacity: 0 },
+                swipeEnabled: false,
+                tabBarGap: 2,
+            }}
         >
             <Tab.Screen name='EnterHabitPage' component={EnterHabitPage} />
             <Tab.Screen name='QuestionPage'component={QuestionPage} />
@@ -35,5 +41,12 @@ export default function CreateHabitLayout() {
 const styles = StyleSheet.create({
     tabBar: {
       backgroundColor: '#586183',
+      paddingBottom: '15%',
     },
+    tabBarItem: {
+        width: 50, 
+    },
+    tabBarContainer: {
+        justifyContent: 'center'
+    }
   });
